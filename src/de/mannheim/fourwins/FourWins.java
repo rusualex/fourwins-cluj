826ec9099c9b98bb47ba1732dd07b445a966339e
rusualex <rusu_alex_geo@yahoo.com>
2018-04-11 13:54:10 +0300

    Commentare

--- src/de/mannheim/fourwins/FourWins.java
+++ src/de/mannheim/fourwins/FourWins.java
@@ -1,24 +1,29 @@
 package de.mannheim.fourwins;
 
-public class FourWins
-{
-  public static void main(String argv[]) {
-    
-    Board theBoard = new Board();
-    
-    ComputerPlayer cp = new ComputerPlayer();
-    cp.putTheBoard( theBoard );
-    
-    AdapterPlayer ap = new AdapterPlayer();
-    
-    GameControler gc = new GameControler();
-    gc.putTheBoard( theBoard );
-    gc.putPlayer( cp, false );
-    gc.putPlayer( ap, true );
-    
-    GameGUI theGUI = new GameGUI(gc,ap);
-    theBoard.register( theGUI );
-    
-    ap.putTheGUI(theGUI);
-  }
+/**
+ * Main Klasse ruft den Board, hort auf den Computer Spieler und auf dem
+ * Computer.
+ * 
+ */
+
+public class FourWins {
+	public static void main(String argv[]) {
+
+		Board theBoard = new Board();
+
+		ComputerPlayer cp = new ComputerPlayer();
+		cp.putTheBoard(theBoard);
+
+		AdapterPlayer ap = new AdapterPlayer();
+
+		GameControler gc = new GameControler();
+		gc.putTheBoard(theBoard);
+		gc.putPlayer(cp, false);
+		gc.putPlayer(ap, true);
+
+		GameGUI theGUI = new GameGUI(gc, ap);
+		theBoard.register(theGUI);
+
+		ap.putTheGUI(theGUI);
+	}
 }
